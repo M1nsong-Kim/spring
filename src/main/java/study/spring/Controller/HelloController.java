51c991de0e51f7c48d1af100c1481c3b1387e784
M1nsong-Kim <lgms.3222@gmail.com>
2023-06-02 20:55:52 +0900

    7. api

--- src/main/java/study/spring/Controller/HelloController.java
+++ src/main/java/study/spring/Controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -19,5 +20,31 @@ public class HelloController {
 	public String helloMvc(@RequestParam("name") String name, Model model) {
 		model.addAttribute("name", name);
 		return "hello-template";
+	}
+	
+	@GetMapping("hello-string")
+	@ResponseBody	// 바디에 내용을 직접 넣겠다(html 소스 부분 x)
+	public String helloString(@RequestParam("name") String name) {
+		return "hello " + name;
+	}
+	
+	@GetMapping("hello-api")
+	@ResponseBody
+	public Hello helloApi(@RequestParam("name") String name) {
+		Hello hello = new Hello();
+		hello.setName(name);
+		return hello;	// 스프링은 객체로 만들면 json 방식으로 반환하는 게 기본 
+	}
+	
+	static class Hello{
+		private String name;
+		
+		public String getName() {
+			return name;
+		}
+		
+		public void setName(String name) {
+			this.name = name;
+		}
 	}
 }
