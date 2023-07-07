cc1fa558c31db19fcc5d6acf1214957580b76739
M1nsong-Kim <lgms.3222@gmail.com>
2023-07-07 10:04:42 +0900

    17. 회원 웹 기능 - 조회

--- src/main/java/study/spring/Controller/MemberController.java
+++ src/main/java/study/spring/Controller/MemberController.java
@@ -1,7 +1,10 @@
 package study.spring.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -52,5 +55,12 @@ public class MemberController {
 		memberService.join(member);
 		
 		return "redirect:/";	// 홈 화면으로 보내기
+	}
+	
+	@GetMapping("/members")
+	public String list(Model model) {
+		List<Member> members = memberService.findMembers();
+		model.addAttribute("members", members);
+		return "members/memberList";
 	}
 }
