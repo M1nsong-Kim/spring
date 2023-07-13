61af0e32b3d606bd4aae8e47d58baf69d9fe7dcd
M1nsong-Kim <lgms.3222@gmail.com>
2023-07-13 22:09:11 +0900

    19. 6 스프링 DB 접근 기술 - 순수 JDBC(2)

--- src/main/java/study/spring/SpringConfig.java
+++ src/main/java/study/spring/SpringConfig.java
@@ -1,15 +1,25 @@
 package study.spring;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import study.spring.Repository.JdbcMemberRepository;
 import study.spring.Repository.MemberRepository;
-import study.spring.Repository.MemoryMemberRepository;
 import study.spring.service.MemberService;
 
 @Configuration
 public class SpringConfig {
 
+	private DataSource dataSource;
+	
+	@Autowired
+	public SpringConfig(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+	
 	@Bean
 	public MemberService memberService() {
 		return new MemberService(memberRepository());
@@ -17,6 +27,7 @@ public class SpringConfig {
 	
 	@Bean
 	public MemberRepository memberRepository() {
-		return new MemoryMemberRepository();
+		// return new MemoryMemberRepository();
+		return new JdbcMemberRepository(dataSource);	// 메모리가 아니라 DB로
 	}
 }
